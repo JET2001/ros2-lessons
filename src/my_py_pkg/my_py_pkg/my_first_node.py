dfa8999e79e2a1f75eca16b591e4fa9b40784549
Jonathan (PC) <jonathan.teo.pc@gmail.com>
2026-08-11 00:33:42 +0800

    Python Node with OOP

--- src/my_py_pkg/my_py_pkg/my_first_node.py
+++ src/my_py_pkg/my_py_pkg/my_first_node.py
@@ -3,15 +3,25 @@
 import rclpy
 from rclpy.node import Node
 
-def main(args=None):
-    rclpy.init(args=args)
-    #
-    node = Node("py_test") # the node is here, its inside the python file
-    node.get_logger().info("Hello world")
-    rclpy.spin(node) #keep the node alive until you press CtrlC
-    
-    #
-    rclpy.shutdown()
+class MyNode(Node):
+    def __init__(self):
+        super().__init__("py_test")
+        
+        self.get_logger().info("Hello world")
+        self.create_timer(1.0, self.timer_callback) # pass the reference to the function
+        self.ctr = 1
+        
+    def timer_callback(self):
+        self.get_logger().info("Hello" + " " + str(self.ctr))
+        self.ctr += 1
 
+def main(args = None):
+    rclpy.init(args=args)
+    node = MyNode()
+    rclpy.spin(node) # this will allow the node to prcoess any callback
+    # callback seems to be triggered only when node is spinning
+    rclpy.shutdown()
+    
+    
 if __name__ == "__main__":
     main()
